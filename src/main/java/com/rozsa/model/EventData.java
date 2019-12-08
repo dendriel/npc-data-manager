ed958895c209ec40b89b8fbb69818d4e0c30d1fd
vitor.rozsa <vitor.rozsa@hotmail.com>
2019-12-07 21:40:45 -0300

    Add Type to EventData

--- src/main/java/com/rozsa/model/EventData.java
+++ src/main/java/com/rozsa/model/EventData.java
@@ -1,28 +1,36 @@
 package com.rozsa.model;
 
-public class EventData
-{
+public class EventData {
+    private EventType type;
+
     private String key;
 
     private Boolean value;
 
-    public String getKey()
-    {
+    public EventType getType() {
+        if (type == null) {
+            type = EventType.SWITCH;
+        }
+        return type;
+    }
+
+    public void setType(EventType type) {
+        this.type = type;
+    }
+
+    public String getKey() {
         return key;
     }
 
-    public void setKey(String key)
-    {
+    public void setKey(String key) {
         this.key = key;
     }
 
-    public Boolean getValue()
-    {
+    public Boolean getValue() {
         return value;
     }
 
-    public void setValue(Boolean value)
-    {
+    public void setValue(Boolean value) {
         this.value = value;
     }
 }
---
+++ src/main/java/com/rozsa/model/EventType.java
@@ -0,0 +1,5 @@
+package com.rozsa.model;
+
+public enum EventType {
+    SWITCH,
+}
